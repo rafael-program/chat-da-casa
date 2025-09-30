634260654e401e89e5e4f4321547301eef7fbdea
rafael-program <rafaelldomiingos552@gmail.com>
2025-09-30 13:18:32 +0100

    fix: add globals.css file

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  experimental: {
-    turbo: {
-      rules: {
-        '*.svg': {
-          loaders: ['@svgr/webpack'],
-          as: '*.js',
-        },
-      },
-    },
-  },
+ 
 };
 
 export default nextConfig;
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -3,7 +3,7 @@
 import { useState } from "react";
 import { supabase } from "../../../lib/supabaseClient";
 import { useRouter } from "next/navigation";
-import "../../globals.css";
+import '../../../app/globals.css'
 
 export default function SignupPage() {
   const [email, setEmail] = useState("");
